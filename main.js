49e9fc6937ea8f5b0a42496f17cd2e85b2f9d2f2
Ridwan <cudancudan71@gmail.com>
2025-11-11 10:42:06 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+function CekNilai() {
+  // mengambil value dari elemen input nilai 
+  // simpan ke dalam varlabel bernama "nilai"
+  let nilai = Number(document.getElementById('nilai').value)
+  
+  //menginisiasi varlabel "hasil" isinya string kosong""
+  let hasil = ""
+  
+  // menentukan angka A B C D berdasarkan isi varlabel "nilai"
+  // menggunakan percabangan if (jika) 
+  // dan membandingkan varlabel "nilai"
+  if (nilai >= 88) {
+    hasil = "nilai A (Sangat Baik)"
+  } else if (nilai >= 75) {
+    hasil = "nilai B (Baik)"
+    
+  }else if (nilai >= 74){
+    hasil = "nilai C (buruk)"
+  }
+  else if (nilai >= 49){
+    hasil = "nilai D (sangat buruk)"
+  }
+  //tampilkan isi variabel "hasil" ke elemen "hasil" di html
+  document.getElementById('hasil').innerText = hasil 
+   
+ 
+}
